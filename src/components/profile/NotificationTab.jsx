9ddca21e7338c7b688af8ef3a0523f8c07eb451a
Munna <munnascriptz@gmail.com>
2026-02-22 21:10:25 +0600

    Notification tab button complete

--- src/components/profile/NotificationTab.jsx
+++ src/components/profile/NotificationTab.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Bell } from 'lucide-react';
 
 const NotificationTab = () => {
+
+  const [notify, setNotify] = useState({
+    newsletter: true,
+    order: false,
+    securityAlert: false,
+    flashAlert: true,
+  })
+
+  const handleNotification = (name) => {
+    setNotify((prev) => ({
+      ...prev,
+      [name]: !prev[name],
+    }))
+  }
+
   return (
     <div className="animate-slide-in-from-bottom-3 duration-500">
       <div className="bg-white rounded-2xl p-10 border border-border shadow-sm">
@@ -13,18 +28,25 @@ const NotificationTab = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-12 gap-y-6">
           {[
-            { label: 'Newsletter & Promos', desc: 'New arrivals and exclusive offers' },
-            { label: 'Order Notifications', desc: 'Updates on your shipping status' },
-            { label: 'Security Alerts', desc: 'Important account login alerts' },
-            { label: 'Flash Sale Entry', desc: 'Early access to limited drops' }
+            { label: 'Newsletter & Promos', name: "newsletter", desc: 'New arrivals and exclusive offers' },
+            { label: 'Order Notifications', name: "order", desc: 'Updates on your shipping status' },
+            { label: 'Security Alerts', name: "securityAlert", desc: 'Important account login alerts' },
+            { label: 'Flash Sale Entry', name: "flashAlert", desc: 'Early access to limited drops' }
           ].map((item, idx) => (
             <div key={idx} className="flex items-center justify-between py-2">
               <div>
                 <h5 className="font-bold text-text-primary text-sm">{item.label}</h5>
                 <p className="text-xs text-text-muted">{item.desc}</p>
               </div>
-              <button className={`w-12 h-6 rounded-full relative cursor-pointer transition-colors p-1 ${idx < 2 ? 'bg-accent' : 'bg-muted'}`}>
-                <div className={`w-4 h-4 bg-white rounded-full shadow-sm shadow-black/20 transition-transform ${idx < 2 ? 'translate-x-6' : 'translate-x-0'}`}></div>
+              <button
+                onClick={() => handleNotification(item.name)}
+                className={`w-12 h-6 rounded-full relative cursor-pointer transition-colors p-1 ${notify[item.name] ? "bg-accent" : "bg-muted"
+                  }`}
+              >
+                <div
+                  className={`w-4 h-4 bg-white rounded-full shadow-sm shadow-black/20 transition-transform ${notify[item.name] ? "translate-x-6" : "translate-x-0"
+                    }`}
+                ></div>
               </button>
             </div>
           ))}

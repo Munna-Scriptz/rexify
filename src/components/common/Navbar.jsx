79cd8baa411fb8358bfce67b60c197b11af3d4b4
Munna <munnascriptz@gmail.com>
2026-02-24 20:22:57 +0600

    Navbar Responsive 70% Complet

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -5,7 +5,8 @@ import { FiHeart } from 'react-icons/fi'
 import { PiUser } from 'react-icons/pi'
 import SearchField from './SearchField'
 import ProductMenu from './ProductMenu'
-import { ShoppingCart, X } from 'lucide-react'
+import { Search, ShoppingCart, X } from 'lucide-react'
+import { HiMiniBars3BottomLeft } from 'react-icons/hi2'
 
 const Navbar = () => {
     const [isNavbarWhite, setIsNavbarWhite] = useState(false);
@@ -65,13 +66,10 @@ const Navbar = () => {
                         <div className="flex md:hidden">
                             <button
                                 onClick={() => setIsMenuOpen(!isMenuOpen)}
-                                className={`p-2 rounded-lg transition-colors ${isNavbarWhite ? 'text-text-primary hover:bg-gray-100' : 'text-white hover:bg-white/10'}`}
+                                className={`p-2 rounded-lg`}
                             >
-                                {isMenuOpen ? (
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                                ) : (
-                                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
-                                )}
+                                <HiMiniBars3BottomLeft className={`w-7 h-7 group-hover:text-text-primary ${isNavbarWhite && 'text-text-primary'}`} />
+
                             </button>
                         </div>
 
@@ -99,7 +97,7 @@ const Navbar = () => {
                         {/* ------------ NavButtons  */}
                         <div className={`md:border-l group-hover:border-text-primary/40 ${isNavbarWhite && 'border-text-primary/40'} border-gray-300 md:pl-3 flex items-center gap-1 md:gap-2 `}>
                             {/* ------- Search  */}
-                            <div onClick={() => { setSearch(!search); setProductMenuOpen(false); }} className='flex items-center rounded-2xl relative cursor-pointer'>
+                            <div onClick={() => { setSearch(!search); setProductMenuOpen(false); }} className='hidden md:flex items-center rounded-2xl relative cursor-pointer'>
                                 <label htmlFor='search' className={`w-8 h-8 pointer-events-none bg-transparent cursor-pointer group-hover:bg-text-muted/20 flex duration-300 items-center justify-center rounded-full absolute left-0`}>
                                     <svg className={`w-5 md:w-6 group-hover:text-text-primary ${isNavbarWhite && 'text-text-primary'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M9.864 3.081A1.56 1.56 0 0 0 9 4.471c0 .275.079.553.215.799a6 6 0 1 0 7.66 6.948a.5.5 0 0 1 .105.183c.235.743.49 1.61 1.418 1.647a8 8 0 0 1-1.126 1.919l4.426 4.317a1 1 0 0 1-1.396 1.432l-4.46-4.348A8 8 0 1 1 9.864 3.081M18.484 8a.3.3 0 0 1 .285.201l.25.766a1.58 1.58 0 0 0 .999.998l.765.248l.015.004a.304.304 0 0 1 .146.46a.3.3 0 0 1-.146.11l-.765.248a1.58 1.58 0 0 0-.999.998l-.249.766a.303.303 0 0 1-.57 0l-.25-.766a1.58 1.58 0 0 0-.998-1.002l-.765-.248a.304.304 0 0 1 .146-.46a.3.3 0 0 1 .146-.11l.765-.248a1.58 1.58 0 0 0 .984-.998L18.2 8.2a.3.3 0 0 1 .284-.2m-4.011-8a.545.545 0 0 1 .512.363l.449 1.376a2.84 2.84 0 0 0 1.797 1.797l1.378.447l.028.007a.55.55 0 0 1 .363.514a.54.54 0 0 1-.363.513l-1.378.447A2.84 2.84 0 0 0 15.46 7.26l-.447 1.376L15 8.67a.545.545 0 0 1-1.014-.034L13.54 7.26a2.84 2.84 0 0 0-1.798-1.804l-1.378-.447A.55.55 0 0 1 10 4.496a.54.54 0 0 1 .363-.513l1.378-.447A2.84 2.84 0 0 0 13.5 1.773l.012-.034l.447-1.376A.55.55 0 0 1 14.473 0"></path>
@@ -107,7 +105,12 @@ const Navbar = () => {
                                 </label>
                                 <input className={`hidden md:block w-30 read pl-10 h-8 outline-none rounded-full hover:bg-text-muted/20 group-hover:placeholder:text-text-primary ${isNavbarWhite && 'placeholder:text-text-primary'} appearance-none duration-300 [&::-webkit-search-cancel-button]:appearance-none cursor-pointer`} readOnly type="search" id='search' name='search' aria-label='search' placeholder='Search' />
                             </div>
+
                             {/* --------- icons  */}
+                            <button onClick={() => { setSearch(!search); setProductMenuOpen(false); }} className={`w-8 h-8 hover:bg-text-muted/20 duration-300 group-hover:text-text-primary ${isNavbarWhite && 'text-text-primary'} text-xl rounded-full cursor-pointer relative`}>
+                                <Search strokeWidth={1} />
+                            </button>
+
                             <Link to={"/cart"} className={`w-8 h-8 flex items-center justify-center hover:bg-text-muted/30 group-hover:text-text-primary duration-300 ${isNavbarWhite && 'text-text-primary'} rounded-full relative`}>
                                 <div className='w-4 h-4 bg-accent rounded-full text-xs text-white flex items-center justify-center absolute -top-1 -right-1'>
                                     <span>3</span>
@@ -116,7 +119,7 @@ const Navbar = () => {
                             </Link>
 
                             {/* --------- Favorites  */}
-                            <Link to={"/wishlist"} className={`w-8 h-8 flex items-center justify-center hover:bg-text-muted/20 duration-300 group-hover:text-text-primary ${isNavbarWhite && 'text-text-primary'} text-xl rounded-full cursor-pointer relative`}>
+                            <Link to={"/wishlist"} className={`w-8 h-8 hidden md:flex items-center justify-center hover:bg-text-muted/20 duration-300 group-hover:text-text-primary ${isNavbarWhite && 'text-text-primary'} text-xl rounded-full cursor-pointer relative`}>
                                 <FiHeart strokeWidth={1.5} />
                             </Link>
 
@@ -130,7 +133,7 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu (Drawer) */}
-                <div className={`md:hidden fixed inset-0 z-[60] transition-opacity duration-300 ${isMenuOpen ? 'opacity-100 visible' : 'opacity-0 invisible pointer-events-none'}`}>
+                <div className={`md:hidden fixed inset-0 z-60 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100 visible' : 'opacity-0 invisible pointer-events-none'}`}>
                     {/* Overlay */}
                     <div className="absolute inset-0 bg-black/50 backdrop-blur-sm" onClick={() => setIsMenuOpen(false)}></div>
 

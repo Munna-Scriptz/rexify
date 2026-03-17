2a19bf63e1f2ed3e196088f41fe0d1427a653af7
Munna <munnascriptz@gmail.com>
2026-03-17 22:46:37 +0600

    Category slider fixing

--- src/components/sliders/CategorySlider.jsx
+++ src/components/sliders/CategorySlider.jsx
@@ -11,28 +11,24 @@ const CategorySlider = ({ products }) => {
     return (
         <div className="relative group">
             <div ref={emblaRef}>
-                <div className="flex gap-12 pb-4">
+                <div className="flex w-full gap-10 pb-4">
                     {products.map((item, index) => (
-                        <div key={index}>
-                            <div className="cursor-pointer flex flex-col items-center gap-3 justify-center pt-2">
-                                {/* Image Container */}
-                                <div className="duration-300 hover:-translate-y-2">
-                                    <img
-                                        src={item.image}
-                                        alt={item.name}
-                                        className="md:w-30 w-25 object-contain"
-                                    />
-                                </div>
-
-                                {/* Product Name */}
-                                <p className="text-sm font-medium text-[#1D1D1F] hover:underline decoration-[#1D1D1F] underline-offset-2">
-                                    {item.name}
-                                </p>
+                        <div className="cursor-pointer flex flex-col items-center gap-3 md:gap-4 justify-center pt-2 select-none shrink-0" key={index}>
+                            {/*Image */}
+                            <div className="duration-300 hover:-translate-y-2">
+                                <img
+                                    src={item.image}
+                                    alt={item.name}
+                                    className="md:w-32 w-28 object-contain"
+                                />
                             </div>
 
+                            {/* Name */}
+                            <p className="text-sm md:text-base font-medium text-[#1D1D1F] hover:underline decoration-[#1D1D1F] underline-offset-2">
+                                {item.name}
+                            </p>
                         </div>
                     ))}
-
                 </div>
             </div>
         </div>

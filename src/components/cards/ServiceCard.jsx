33d0a9f71647ed75a448ee7bd484b1e2107aa324
Munna <munnascriptz@gmail.com>
2026-03-17 23:44:04 +0600

    Category slider fixed and all sliders complete

--- src/components/cards/ServiceCard.jsx
+++ src/components/cards/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCard = ({ item }) => {
     return (
-        <div className=" group relative p-6 h-full bg-linear-to-br from-white to-gray-50 rounded-3xl border border-gray-100 hover:border-gray-200 shadow-sm hover:shadow-lg transition-all duration-500 ease-out hover:bg-linear-to-br hover:from-white hover:to-blue-50 overflow-hidden">
+        <div className=" group relative md:p-6 p-5 h-full bg-linear-to-br from-white to-gray-50 rounded-3xl border border-gray-100 hover:border-gray-200 shadow-sm hover:shadow-lg transition-all duration-500 ease-out hover:bg-linear-to-br hover:from-white hover:to-blue-50 overflow-hidden">
             {/* Decorative Background Blur */}
             <div className="absolute -top-20 -right-20 w-40 h-40 bg-blue-100 rounded-full opacity-0 group-hover:opacity-10 transition-opacity duration-500 blur-3xl" />
 
@@ -11,17 +11,17 @@ const ServiceCard = ({ item }) => {
 
             {/* Icon - Clean & Minimal */}
             <div className="flex items-center gap-3">
-                <div className="relative z-10 flex items-center justify-center w-14 h-14 rounded-2xl bg-linear-to-br from-blue-50 to-blue-100 text-blue-600 mb-6 group-hover:scale-105 group-hover:shadow-md transition-all duration-500">
+                <div className="relative z-10 flex items-center justify-center md:w-14 w-12 md:h-14 h-12 rounded-2xl bg-linear-to-br from-blue-50 to-blue-100 text-blue-600 mb-6 group-hover:scale-105 group-hover:shadow-md transition-all duration-500">
                     <item.icon size={28} strokeWidth={1.5} />
                 </div>
-                <h3 className="text-lg font-semibold text-coil mb-3 tracking-tight">
+                <h3 className="md:text-lg text-base font-semibold text-coil mb-3 tracking-tight">
                     {item.title}
                 </h3>
             </div>
 
             {/* Text */}
             <div className="relative z-10">
-                <p className="text-text-secondary text-sm leading-relaxed font-normal select-none">
+                <p className="text-text-secondary md:text-sm text-xs leading-relaxed font-normal select-none">
                     {item.desc}
                 </p>
             </div>
--- src/components/sliders/CategorySlider.jsx
+++ src/components/sliders/CategorySlider.jsx
@@ -11,23 +11,22 @@ const CategorySlider = ({ products }) => {
     return (
         <div className="relative group">
             <div ref={emblaRef}>
-                <div className="flex gap-10 pb-4">
+                <div className="flex md:gap-10 gap-6 pb-4">
                     {products.map((item, index) => (
-                        <div
-                            key={index}
-                            className="shrink-0 basis-22 md:basis-35.5 flex flex-col items-center gap-3 md:gap-4 justify-center pt-2 select-none cursor-pointer"
-                        >
-                            <div className="duration-300 hover:-translate-y-2">
-                                <img
-                                    src={item.image}
-                                    alt={item.name}
-                                    className="w-full object-contain"
-                                />
-                            </div>
+                        <div key={index} className="shrink-0 basis-22 md:basis-35.5">
+                            <div className='select-none cursor-pointer flex flex-col items-center gap-3 md:gap-4 justify-center pt-2'>
+                                <div className="duration-300 hover:-translate-y-2">
+                                    <img
+                                        src={item.image}
+                                        alt={item.name}
+                                        className="w-full object-contain"
+                                    />
+                                </div>
 
-                            <p className="text-sm md:text-base font-medium text-[#1D1D1F] hover:underline underline-offset-2">
-                                {item.name}
-                            </p>
+                                <p className="text-sm md:text-base font-medium text-[#1D1D1F] hover:underline underline-offset-2">
+                                    {item.name}
+                                </p>
+                            </div>
                         </div>
                     ))}
                 </div>

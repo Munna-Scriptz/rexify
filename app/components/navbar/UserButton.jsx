4a9613c210f10e9b865440ff8b80acfee4bb5993
Munna <munnascriptz@gmail.com>
2026-04-23 17:20:23 +0600

    User button profile data fetched

--- app/components/navbar/UserButton.jsx
+++ app/components/navbar/UserButton.jsx
@@ -1,11 +1,23 @@
 "use client"
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const UserButton = ({ isNavbarWhite }) => {
-    const [user, setUser] = useState("user")
+    const [user, setUser] = useState("")
+    const [data, setData] = useState(null);
 
-
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/profile`, {
+            method: "GET",
+            credentials: "include",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                setUser(data.role)
+            })
+            .catch((err) => console.error(err));
+    }, []);
     return (
         <>
             <Link
@@ -15,7 +27,7 @@ const UserButton = ({ isNavbarWhite }) => {
                 {/* Avatar */}
                 {
                     !user ?
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`w-4 h-4 `} fill="none" viewBox="0 0 24 24" stroke="currentColor" ><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A9 9 0 1118.364 6.636M15 11h4m0 0v4m0-4l4" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="M11.47 8.47a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1 0 1.06l-3 3a.75.75 0 1 1-1.06-1.06l1.72-1.72H4a.75.75 0 0 1 0-1.5h9.19l-1.72-1.72a.75.75 0 0 1 0-1.06" opacity={0.5}></path><path fill="currentColor" d="M13 5.75c-1.703 0-3.246.68-4.374 1.786a.75.75 0 0 1-1.05-1.072a7.75 7.75 0 1 1 0 11.071a.75.75 0 0 1 1.05-1.07A6.25 6.25 0 1 0 13 5.75"></path></svg>
                         :
                         <img
                             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo6wCUSmJEK9kC5KVqmQczHMH3OMcc_9BTTQ&s"
@@ -26,7 +38,7 @@ const UserButton = ({ isNavbarWhite }) => {
 
                 {/* Name */}
                 <span className={`text-sm font-medium`}>
-                    {!user ? "Sign In" : user === "admin" ? "Admin" : "John Doe"}
+                    {!user ? "Sign In" : user === "admin" ? "Admin" : data.fullname}
                 </span>
 
                 {/* Icon */}

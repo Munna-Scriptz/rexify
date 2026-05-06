89523cd59f012cbaaa7fe225a708927c48be0ff7
Munna <munnascriptz@gmail.com>
2026-05-06 22:22:37 +0600

    Featured products component added

--- client/app/(main)/page.jsx
+++ client/app/(main)/page.jsx
@@ -3,6 +3,7 @@ import Banner from "../components/home/Banner"
 import Services from "../components/home/Services"
 import SmallCategory from "../components/home/SmallCategory"
 import LatestProducts from "../components/home/LatestProducts"
+import Featured from '../components/home/Featured'
 import BestSeller from "../components/home/BestSeller"
 import MarqueeText from "../components/home/MarqueeText"
 import Essentials from "../components/home/Essentials"
@@ -15,6 +16,7 @@ export default function Home() {
       <Services />
       <SmallCategory />
       <LatestProducts />
+      <Featured />
       <BestSeller />
       <MarqueeText />
       <Essentials />
